6de5fad2c0349aebb8f1cc53bfd08d71f00fd8d5
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2025-09-12 06:24:28 +0500

    Arrays

---
+++ Arrayss/Insertion_at_end.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[100]={12,34,50,100}, value;
+
+    int size=4;
+
+    cout<<"Enter the value to insert at the end "<<endl;
+    cin>>value;
+
+
+    arr[size]= value;
+
+    size++;
+
+
+    for(int i=0; i<size; i++){
+
+        cout<<arr[i]<<"  ";
+
+    }
+
+    cout<<endl;
+
+
+
+    
+    return 0;
+}
---
+++ Arrayss/Insertion_at_start.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[100]={12,34,100,50},num;
+
+    int size=4;
+
+   
+    cout<<"Enter the value to insert in the start "<<endl;
+cin>>num;
+
+for( int i=size;i>0;i--)
+{
+
+    arr[i]=arr[i-1];
+}
+
+arr[0]=num;
+
+size++;
+
+for(int i=0; i<size; i++){
+
+    cout<<arr[i]<<"  ";
+
+
+}
+
+cout<<endl;
+
+    
+    return 0;
+}
